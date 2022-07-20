1dc3110afac6c68e94f40d9ff57ce16753c7694c
xenant <xenant.inc@gmail.com>
2022-07-20 09:58:36 -0700

    Finished practice problem 1

---
+++ Practice.py
@@ -0,0 +1,18 @@
+#https://www.practicepython.org/exercise/2014/01/29/01-character-input.htmlname = input("Please type in in your name! : ")
+name = input("Please type in in your name! :")
+print("Your name is..." + name + "!") 
+
+age = int(input("Please type your age:"))
+print("You inputted " + str(age))
+
+currentYear = 2022 
+
+outputNum = currentYear + 100 
+
+finalAge = 100 - age 
+
+print( name + " will be 100 years old on " + str(currentYear + finalAge) )
+
+
+
+# name will be 100 years old on 
